9c1ef95c95b98fd6a69b67f65bf74fda913c4636
Wan-Yi Yeh <wyeh2@ncsu.edu>
2018-02-21 03:39:16 -0500

    #9 auto scroll down

--- jsBot/src/main.js
+++ jsBot/src/main.js
@@ -3,7 +3,7 @@ var Chat = function() {
     var request = new XMLHttpRequest();
     var topic = "";
     request.open("GET", "data/profile.json", false);
-    request.send(null)
+    request.send(null);
     var profile = JSON.parse(request.responseText);
 
 
@@ -14,8 +14,11 @@ var Chat = function() {
         message.className = bot ? 'message bot' : 'message';
         message.innerHTML = text;
 
+        //animate
         setTimeout(function(){
             messages.appendChild(message);
+            var objDiv = document.getElementById("chatText");
+            objDiv.scrollTop = objDiv.scrollHeight;
         }, delay);
     }
 
@@ -31,6 +34,7 @@ var Chat = function() {
     }
 
     function handleInput(input) {
+
         switch(topic){
             case "interest":
                 profile.interest.answer = input;
@@ -58,6 +62,7 @@ var Chat = function() {
                 talk();
                 break;
         }
+
     }
 
     function talk() {
@@ -78,51 +83,49 @@ var Chat = function() {
         }
         else {
             topic = "other";
-            chat();
+            other();
         }
 
+
+        //scrollDown();
 
 
     }
 
-    function chat() {
-        output("If you need course suggestion for your interested subject, type \"suggest\"." +
-            "If you want to see detail of specific course, type \"course name\"", true);
+    function other() {
+        output("Type \"suggest\" for suggest course based on your interesting subject, " +
+            "or type \"course name\" for course detail", true, 500);
+        console.log("other");
     }
 
     function requiredInt() {
         var question = profile.interest.question;
-        output(question, true);
+        output(question, true, 500);
     }
 
     function requiredUgg() {
         var question = profile.ugg.question;
-        output(question, true);
+        output(question, true, 500);
     }
 
     function requiredPro() {
         var question = profile.project.question;
-        output(question, true);
+        output(question, true, 500);
     }
 
-    function sample() {
-        output('', true, 1000);
+    function suggestion() {
+        output("give suggestion", true, 500);
     }
+    function detail(course) {
+        output("give detail of " + course , true, 500);
+    }
+
+
 
     function init() {
         output('Hey pal, I can help you with course selection, if you tell me a bit about yourself.', true);
         talk();
     }
-
-
-    function suggestion() {
-        output("give suggestion", true);
-    }
-    function detail(course) {
-        output("give detail", true);
-    }
-
-
 
     return {
         init: init,
