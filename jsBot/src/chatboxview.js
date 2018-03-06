f9485fbbdb58b92377b7bd066a5ef101bbe3d558
Kapil Chopra <kapilchopra963@gmail.com>
2018-03-06 00:03:57 -0500

    Chatting button added
    Button for toggling the chat window added.

---
+++ jsBot/src/chatboxview.js
@@ -0,0 +1,8 @@
+function chatwindowshow() {
+    document.getElementsByName("chatwindow")[0].style.display = "flex";
+    document.getElementsByName("botbutton")[0].style.display = "none";
+}
+function chatwindowhide() {
+    document.getElementsByName("chatwindow")[0].style.display = "none";
+    document.getElementsByName("botbutton")[0].style.display = "block";
+}
